6fa97c0fb86e5062a72889b0df19f44dd595a1c5
vicente dauce <dauce@ua.pt>
2019-08-21 16:26:24 +0100

    restaurant mvc

--- db/migrate/20190821110934_create_restaurants.rb
+++ db/migrate/20190821110934_create_restaurants.rb
@@ -5,7 +5,6 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
       t.string :address
       t.string :phone_number
       t.string :category
-
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+niao = Restaurant.create!(name: "niao", address: "china", category: "chinese", phone_number: "002589245")
+romero = Restaurant.create!(name: "romero", address: "lisbon", category: "italian", phone_number: "00323225")
+lukaku = Restaurant.create!(name: "lukaku", address: "aveiro", category: "belgian", phone_number: "003239245")
+bonjour = Restaurant.create!(name: "bonjour", address: "braga", category: "french", phone_number: "0027899245")
+arigato = Restaurant.create!(name: "arigato", address: "japan", category: "japanese", phone_number: "00894735")
